49de7d13e588817b5a10f20d17c7ca30825e3160
Justin <justinliwo@gmail.com>
2017-07-16 08:00:52 +0000

    connected to DynamoDB API's

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 'use strict'
 const http = require('http')
 const Bot = require('messenger-bot')
+var express = require('express');
+var app = express();
+
 var fs = require('fs'),
     request = require('request');
+
+app.get('/', (req, res) => {
+    deleteBillDynamo("testUserId", function(data){
+        res.send(data);
+    });
+});
 
 let bot = new Bot({
   token: 'EAAUvNzGGhXkBADTjO1a7cIDEg17OUpCJw3mjCaM7xAP1ShiUQriZBFTvf40JVRrqMrj6WmyFAkTdTKag7jHFSo3tUTcyTMcnfqg24tkY8LH58Y4twCHQZBtioX49QnNkmiOvrp3zBxnQXnoZAyUP8kDdjOJZBSmdU8ZBoxdQTegZDZD',
@@ -53,7 +62,7 @@ bot.on('message', (payload, reply) => {
   }
 })
 
-var getBillDynamo = function(id){
+var getBillDynamo = function(id, callback){
     //list should be a JSON structure that you call .toString on
 
     var options = { method: 'GET',
@@ -66,11 +75,62 @@ var getBillDynamo = function(id){
 
     request(options, function (error, response, body) {
         if (error) throw new Error(error);
-
-        return body;
+        callback(body);
     });
+};
 
+var putBillDynamo = function(id, list, callback){
+var options = { method: 'POST',
+  url: 'http://ec2-52-25-176-24.us-west-2.compute.amazonaws.com:3000/putBill',
+  body: { userId: id, list: list },
+  json: true };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+      callback(body);
+  });
+}
+
+var deleteBillDynamo = function(id, callback){
+  var options = { method: 'DELETE',
+  url: 'http://ec2-52-25-176-24.us-west-2.compute.amazonaws.com:3000/deleteBill',
+  qs: { userId: 'tester' },
+  headers: 
+   { 'postman-token': '6c877626-45e8-bb7c-07c8-5cf108cfbdb5',
+     'cache-control': 'no-cache',
+     'content-type': 'application/json',
+     'x-fullcontact-apikey': '583052e807c0615d' },
+  body: { userId: '12345', list: 'dddd' },
+  json: true };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  callback(body);
+});
+}
+
+var incrementBillDynamo = function(id, callback) {
+  var options = { method: 'POST',
+  url: 'http://ec2-52-25-176-24.us-west-2.compute.amazonaws.com:3000/incrementBill',
+  headers: 
+   { 'postman-token': '14c37120-f902-e03e-7518-52060dffebfd',
+     'cache-control': 'no-cache',
+     'content-type': 'application/json',
+     'x-fullcontact-apikey': '583052e807c0615d' },
+  body: { userId: id },
+  json: true };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  callback(body);
+});
 
 }
 
-http.createServer(bot.middleware()).listen(443)
+app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
+  console.log("Server started");
+});
+
+// http.createServer(bot.middleware()).listen(3000)
